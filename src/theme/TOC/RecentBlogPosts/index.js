f6279fa829eb1eca26c29534dc632419efb86284
guchimon99 <guchimon99@gmail.com>
2022-04-22 18:27:55 +0900

    [UPDATE] タグの変更に伴うサイト全体の共通部分の変更

--- src/theme/TOC/RecentBlogPosts/index.js
+++ src/theme/TOC/RecentBlogPosts/index.js
@@ -6,7 +6,7 @@ import styles from './styles.module.css'
 
 import {
   useNewsPosts,
-  useTroublereportPosts
+  useMaintenancePosts
 } from './hooks'
 
 const RECENT_POST_COUNT = 5
@@ -21,10 +21,10 @@ function ListItem ({ post }) {
 
 export default function RecentBlogPosts () {
   const newsPosts = useNewsPosts()
-  const troubleReportPosts = useTroublereportPosts()
+  const maintenancePosts = useMaintenancePosts()
 
   const recentNewsPosts = newsPosts.slice(0, RECENT_POST_COUNT)
-  const recentTroubleReportPosts = troubleReportPosts.slice(0, RECENT_POST_COUNT)
+  const recentMaintenancePosts = maintenancePosts.slice(0, RECENT_POST_COUNT)
 
   return (
     <div className={styles.recentBlogPosts}>
@@ -45,15 +45,15 @@ export default function RecentBlogPosts () {
       </section>
       <section className={styles.recentBlogPosts__section}>
         <h2 className={styles.recentBlogPosts__title}>
-          <Translate id="themes.recentBlogPosts.recentTroubleReportPosts">最近の障害報告</Translate>
+          <Translate id="themes.recentBlogPosts.recentMaintenancePosts">メンテナンス情報</Translate>
         </h2>
         <ul className={styles.recentBlogPosts__list}>
-          {recentTroubleReportPosts.map(post => (
+          {recentMaintenancePosts.map(post => (
             <ListItem key={post.id} post={post} />
           ))}
           <li className={styles.recentBlogPosts__listItem}>
-            <Link to="/blog/tags/troublereport" className={styles.recentBlogPosts__listItemLink}>
-              <Translate id="themes.recentBlogPosts.moreTroubleReportPosts">すべての障害報告を見る</Translate>
+            <Link to="/blog/tags/maintenance" className={styles.recentBlogPosts__listItemLink}>
+              <Translate id="themes.recentBlogPosts.moreMaintenancePosts">すべてのメンテナンス情報を見る</Translate>
             </Link>
           </li>
         </ul>
